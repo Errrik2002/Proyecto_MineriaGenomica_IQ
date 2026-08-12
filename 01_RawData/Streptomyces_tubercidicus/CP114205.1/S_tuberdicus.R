5817fca8ffa5218e3d459e98633ff5283b962d96
Erik Martínez <119984425+Errrik2002@users.noreply.github.com>
2026-08-12 14:53:37 -0600

    ejemplos antismash

---
+++ 01_RawData/Streptomyces_tubercidicus/CP114205.1/S_tuberdicus.R
@@ -0,0 +1,10 @@
+#No carga geneviewer, no se por que
+
+#install.packages('geneviewer', repos = c('https://nvelden.r-universe.dev', 'https://cloud.r-project.org'))
+#install.packages("ggplot2")
+install.packages("devtools")
+
+
+library(devtools)
+library(ggplot2)
+library(geneviewer)

1119e7aa76a884504a7406e8d03a1395646e33de
Erik Martínez <119984425+Errrik2002@users.noreply.github.com>
2026-08-13 12:45:16 -0600

    analisis de resultados de AntiSMASH con R

--- devtools.R
+++
@@ -1,6 +0,0 @@
-install.packages("devtools")
-
-
-library(devtools)
-
-library(geneviewer)
---
+++ mibig_analisis.R
@@ -0,0 +1,7 @@
+library(geneviewer)
+
+mibig_results_reg03 <- "03_Results/results_antismash/Gen_BankCP114205.1/CP114205.1.region003/"
+
+mibig_R3 <- read_gbk("03_Results/results_antismash/Gen_BankCP114205.1/CP114205.1.region003/CP114205.1.region003.gbk")
+mibig_R3
+class(mibig_R3)
